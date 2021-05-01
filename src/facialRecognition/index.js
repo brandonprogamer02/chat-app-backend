91310e842b1d6a0dff351a17e657419b6c433ab8
brandon <brandonprogamer02@gmail.com>
2021-05-01 02:12:51 -0400

    resolving paths 2

--- src/facialRecognition/index.js
+++ src/facialRecognition/index.js
@@ -4,7 +4,7 @@ import canvas, { Canvas, Image } from 'canvas';
 import UserModel from '../models/UserModel';
 
 export async function facialReconitionWithStorageImage(inputImage) {
-     const MODEL_URL = path.join(__dirname,'facialRecognition' ,'models');
+     const MODEL_URL = path.join(__dirname,'models');
 
      try {
           await Promise.all([
