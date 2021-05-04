caa8b935cde8c0daa9a624138ee341040c17714e
brandon <brandonprogamer02@gmail.com>
2021-05-04 18:37:06 -0400

    :)

--- src/controllers/authControllers/signController/signController.ts
+++ src/controllers/authControllers/signController/signController.ts
@@ -1,6 +1,5 @@
 import { RequestHandler } from "express";
 import { ICreateUserAndTokenJWT, IUser, IUserSign } from "../../../types";
-import { verifyIfExistFaceAndIfIsBelongToOtherUser } from '../../../facialRecognition/utils'
 import { createUserAndTokenJWT } from "../../../auth";
 
 const signController: RequestHandler = async (req, res, next): Promise<void> => {
@@ -11,32 +10,10 @@ const signController: RequestHandler = async (req, res, next): Promise<void> => 
      else if (!password) res.status(400).send('The property "password" has not been provided');
      else {
 
-          const { dataRecognition, facesDetecting } = await verifyIfExistFaceAndIfIsBelongToOtherUser(userLogImage);
-          // if do not match continue
-          console.log(facesDetecting)
-          if (facesDetecting == 0) {
-               res.json({
-                    token: null,
-                    faceDetecting: facesDetecting == 0 ? false : true,
-                    faceRecognition: false
-               })
-          }else if (!dataRecognition) {
-               // create the user and tok    en
-               const resJWT: ICreateUserAndTokenJWT = await createUserAndTokenJWT({ username, password, userLogImage });
-               // const userId = resJWT.userCreated._id as string;
-               // response to client
-               res.json({
-                    token: resJWT.token,
-                    faceDetecting: facesDetecting == 0 ? false : true,
-                    faceRecognition: false
-               });
-          } else {
-               res.json({
-                    token: null,
-                    faceDetecting: facesDetecting == 0 ? false : true,
-                    faceRecognition: true
-               });
-          }
+          // const { dataRecognition, facesDetecting } = await verifyIfExistFaceAndIfIsBelongToOtherUser(userLogImage);
+          const { token, userCreated }: ICreateUserAndTokenJWT = await createUserAndTokenJWT({ username, password, userLogImage });
+
+          res.json({ token, user: userCreated });
      }
 
 }
